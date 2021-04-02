00671e7bc2401cb57707b456763d18a1a3c57518
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-04-02 00:09:36 -0400

    Updated logo and removed unnecessary files

--- routes/investors.js
+++ routes/investors.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 /* GET users listing. */
-router.get('/investors', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render("investors", { title: "This is investors page" });
 });
 
