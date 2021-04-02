5faf199b6692d53593f618e68063e143379968f4
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-04-02 15:32:45 -0400

    Added Registration Pages

--- routes/businesses.js
+++ routes/businesses.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render("businesses", { title: "This is Businesses page" });
 });
 
+router.get('/busregister', function(req, res, next) {
+    res.render("businessRegister", { title: "This is Businesses registraion page" });
+});
+
 module.exports = router;
--- routes/investors.js
+++ routes/investors.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render("investors", { title: "This is investors page" });
 });
 
+router.get('/invregister', function(req, res, next) {
+    res.render("investorRegister", { title: "This is investors registration page" });
+});
+
 module.exports = router;
